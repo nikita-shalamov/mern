bfc7d20132be706499382c07248cfbca1f4ac728
Nikita Shalamov <nikita.shalamovv@yandex.ru>
2024-07-26 16:42:33 +0500

    updated

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ import mongoose from 'mongoose';
 import routerAuth from './routes/auth.routes.js'; // Correct path to auth.routes.js
 import routerLink from './routes/link.routes.js';
 import redirectRouter from './routes/redirect.routes.js';
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use('/api/auth', routerAuth);
-app.use('/api/link', routerLink)
-app.use('/t', redirectRouter)
+app.use('/api/link', routerLink);
+app.use('/t', redirectRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-} 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = config.get('port') || 3000;
 
